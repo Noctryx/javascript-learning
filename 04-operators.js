87ad17ed73689e92293ec79f0181f40b971d79e7
Venky <venky14182007@gmail.com>
2026-03-09 19:18:51 +0530

    Day-4 learned operators

---
+++ 04-operators.js
@@ -0,0 +1,47 @@
+//assignment operators
+let x = 10;
+let y = 5;
+
+//arithmetic operators
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+console.log(x++);
+console.log(++x);
+console.log(x--);
+console.log(--x);
+
+//assignment operators
+x += 5;
+console.log(x);
+x -= 5;
+console.log(x);
+x *= 5;
+console.log(x);
+x /= 5;
+console.log(x);
+x **= 5;
+console.log(x);
+x %= 5;
+console.log(x);
+
+//comparison operators
+console.log(x == y);
+console.log(x === y);
+console.log(x != y);
+console.log(x !== y);
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y);
+
+//logical operators
+let a = true;
+let b = false;
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+console.log(!b);
